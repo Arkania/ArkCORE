706824d867ddc16f1931283c913aff2f3d8b3a49
xKaixer <xkaixer@gmail.com>
2011-12-16 23:41:14 +0800

    DB/World Remove Spell Ranck not in game. -- By Redneo

---
+++ sql/updates/world/11_12_16_01_world_spell_ranks.sql
@@ -0,0 +1,5 @@
+-- Spell Rank http://www.wowhead.com/spell=74497
+-- By Redneo
+delete from `spell_ranks` where `spell_id` = 74497;
+ 
+insert into `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) values('81708','74497','8');
