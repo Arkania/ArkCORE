e9dea01798849a2721d2c34b6989b5f19362f2d6
Spyder <serge@arkania.net>
2011-12-20 11:07:42 +0800

    Core/Scripts: Gundrak instance updated.

--- src/server/scripts/Northrend/Gundrak/boss_eck.cpp
+++ src/server/scripts/Northrend/Gundrak/boss_eck.cpp
@@ -1,170 +1,247 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://www.getmangos.com/>
- *
- * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
- *
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
- *
  * Copyright (C) 2010-2011 ProjectSkyfire <http://www.projectskyfire.org/>
  * 
  * Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ScriptPCH.h"
 #include "gundrak.h"
 
-enum Spells {
-	SPELL_ECK_BERSERK = 55816, //Eck goes berserk, increasing his attack speed by 150% and all damage he deals by 500%.
-	SPELL_ECK_BITE = 55813, //Eck bites down hard, inflicting 150% of his normal damage to an enemy.
-	SPELL_ECK_SPIT = 55814, //Eck spits toxic bile at enemies in a cone in front of him, inflicting 2970 Nature damage and draining 220 mana every 1 sec for 3 sec.
-	SPELL_ECK_SPRING_1 = 55815, //Eck leaps at a distant target.  --> Drops aggro and charges a random player. Tank can simply taunt him back.
-	SPELL_ECK_SPRING_2 = 55837
-//Eck leaps at a distant target.
+enum Spells
+{
+    SPELL_ECK_BERSERK                             = 55816, //Eck goes berserk, increasing his attack speed by 150% and all damage he deals by 500%.
+    SPELL_ECK_BITE                                = 55813, //Eck bites down hard, inflicting 150% of his normal damage to an enemy.
+    SPELL_ECK_SPIT                                = 55814, //Eck spits toxic bile at enemies in a cone in front of him, inflicting 2970 Nature damage and draining 220 mana every 1 sec for 3 sec.
+    SPELL_ECK_SPRING_1                            = 55815, //Eck leaps at a distant target.  --> Drops aggro and charges a random player. Tank can simply taunt him back.
+    SPELL_ECK_SPRING_2                            = 55837,  //Eck leaps at a distant target.
+    //ruins dweller spells
+    SPELL_REGURGITATE                             = 55643,
+    SPELL_SPRING                                  = 55652
 };
 
-static Position EckSpawnPoint = { 1643.877930f, 936.278015f, 107.204948f,
-		0.668432f };
+static Position EckSpawnPoint = 
+{ 
+    1643.877930f, 936.278015f, 107.204948f, 0.668432f 
+};
 
-class boss_eck: public CreatureScript {
+class boss_eck : public CreatureScript
+{
 public:
-	boss_eck() :
-			CreatureScript("boss_eck") {
-	}
+    boss_eck() : CreatureScript("boss_eck") { }
 
-	CreatureAI* GetAI(Creature* pCreature) const {
-		return new boss_eckAI(pCreature);
-	}
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new boss_eckAI (creature);
+    }
 
-	struct boss_eckAI: public BossAI {
-		boss_eckAI(Creature *c) :
-				BossAI(c, DATA_ECK_THE_FEROCIOUS_EVENT) {
-			me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK,
-					true);
-			me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_GRIP, true);
-		}
+    struct boss_eckAI : public ScriptedAI
+    {
+        boss_eckAI(Creature* c) : ScriptedAI(c)
+        {
+            pInstance = c->GetInstanceScript();
+        }
 
-		uint32 uiBerserkTimer;
-		uint32 uiBiteTimer;
-		uint32 uiSpitTimer;
-		uint32 uiSpringTimer;
+        uint32 uiBerserkTimer;
+        uint32 uiBiteTimer;
+        uint32 uiSpitTimer;
+        uint32 uiSpringTimer;
 
-		bool bBerserk;
+        bool bBerserk;
 
-		void Reset() {
-			uiBerserkTimer = urand(60 * IN_MILLISECONDS, 90 * IN_MILLISECONDS); //60-90 secs according to wowwiki
-			uiBiteTimer = 5 * IN_MILLISECONDS;
-			uiSpitTimer = 10 * IN_MILLISECONDS;
-			uiSpringTimer = 8 * IN_MILLISECONDS;
+        InstanceScript* pInstance;
 
-			bBerserk = false;
+        void Reset()
+        {
+            uiBerserkTimer = urand(60*IN_MILLISECONDS, 90*IN_MILLISECONDS); //60-90 secs according to wowwiki
+            uiBiteTimer = 5*IN_MILLISECONDS;
+            uiSpitTimer = 7*IN_MILLISECONDS;
+            uiSpringTimer = 12*IN_MILLISECONDS;
 
-			if (instance)
-				instance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, NOT_STARTED);
-		}
+            bBerserk = false;
 
-		void EnterCombat(Unit* /*who*/) {
-			if (instance)
-				instance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, IN_PROGRESS);
-		}
+            if (pInstance)
+                pInstance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, NOT_STARTED);
+        }
 
-		void UpdateAI(const uint32 diff) {
-			//Return since we have no target
-			if (!UpdateVictim())
-				return;
+        void EnterCombat(Unit* /*who*/)
+        {
+            if (pInstance)
+                pInstance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, IN_PROGRESS);
+        }
 
-			if (uiBiteTimer <= diff) {
-				DoCast(me->getVictim(), SPELL_ECK_BITE);
-				uiBiteTimer = urand(8 * IN_MILLISECONDS, 12 * IN_MILLISECONDS);
-			} else
-				uiBiteTimer -= diff;
+        void EckResetThread(Unit* victim)
+        {
+            std::list<HostileReference*>& threatlist = me->getThreatManager().getThreatList();
 
-			if (uiSpitTimer <= diff) {
-				DoCast(me->getVictim(), SPELL_ECK_SPIT);
-				uiSpitTimer = urand(6 * IN_MILLISECONDS, 14 * IN_MILLISECONDS);
-			} else
-				uiSpitTimer -= diff;
+            for (std::list<HostileReference*>::iterator itr = threatlist.begin(); itr != threatlist.end(); ++itr)
+            {
+                Unit* pUnit = Unit::GetUnit((*me), (*itr)->getUnitGuid());
 
-			if (uiSpringTimer <= diff) {
-				Unit* pTarget = SelectTarget(SELECT_TARGET_RANDOM, 1);
-				if (pTarget && pTarget->GetTypeId() == TYPEID_PLAYER) {
-					DoCast(pTarget,
-							RAND(SPELL_ECK_SPRING_1, SPELL_ECK_SPRING_2));
-					uiSpringTimer = urand(5 * IN_MILLISECONDS,
-							10 * IN_MILLISECONDS);
-				}
-			} else
-				uiSpringTimer -= diff;
+                if (pUnit && DoGetThreat(pUnit))
+                {
+                    DoModifyThreatPercent(pUnit, -100);
+                    me->AddThreat(pUnit, 0.1f);
+                }
+            }
+            
+            //needed?
+            if (victim && victim->isAlive()) 
+            {
+                AttackStart(victim);
+                me->AddThreat(victim, 10.0f);
+            }
+        }
 
-			//Berserk on timer or 20% of health
-			if (!bBerserk) {
-				if (uiBerserkTimer <= diff) {
-					DoCast(me, SPELL_ECK_BERSERK);
-					bBerserk = true;
-				} else {
-					uiBerserkTimer -= diff;
-					if (HealthBelowPct(20)) {
-						DoCast(me, SPELL_ECK_BERSERK);
-						bBerserk = true;
-					}
-				}
-			}
+        void UpdateAI(const uint32 diff)
+        {
+            //Return since we have no target
+            if (!UpdateVictim())
+                return;
 
-			DoMeleeAttackIfReady();
-		}
+            if (uiBiteTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_ECK_BITE);
+                uiBiteTimer = urand(8*IN_MILLISECONDS,12*IN_MILLISECONDS);
+            } else uiBiteTimer -= diff;
 
-		void JustDied(Unit* /*killer*/) {
-			_JustDied();
-			if (instance)
-				instance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, DONE);
-		}
-	};
+            if (uiSpitTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_ECK_SPIT);
+                uiSpitTimer = urand(11*IN_MILLISECONDS,20*IN_MILLISECONDS);
+            } else uiSpitTimer -= diff;
+
+            if (uiSpringTimer <= diff)
+            {
+                Unit* target = SelectTarget(SELECT_TARGET_RANDOM,1);
+                if (target && target->GetTypeId() == TYPEID_PLAYER)
+                {
+                    if (me->GetExactDist(target->GetPositionX(), target->GetPositionY(), target->GetPositionZ()) < 35)
+                    { 
+                    DoCast(target, RAND(SPELL_ECK_SPRING_1, SPELL_ECK_SPRING_2));
+                        EckResetThread(target); //test
+
+                        uiSpringTimer = urand(9*IN_MILLISECONDS, 15*IN_MILLISECONDS);
+                    }
+                }
+            } else uiSpringTimer -= diff;
+
+            if (!bBerserk)
+            {
+                if (uiBerserkTimer <= diff)
+                {
+                    me->InterruptNonMeleeSpells(true);
+                    DoCast(me, SPELL_ECK_BERSERK);
+                    bBerserk = true;
+                }
+                else uiBerserkTimer -= diff;
+            }
+
+            DoMeleeAttackIfReady();
+        }
+
+        void JustDied(Unit* /*killer*/)
+        {
+            if (pInstance)
+                pInstance->SetData(DATA_ECK_THE_FEROCIOUS_EVENT, DONE);
+        }
+    };
+
 };
 
-class npc_ruins_dweller: public CreatureScript {
+#define RANGE_ECK 1000.0f
+
+class npc_ruins_dweller : public CreatureScript
+{
 public:
-	npc_ruins_dweller() :
-			CreatureScript("npc_ruins_dweller") {
-	}
+    npc_ruins_dweller() : CreatureScript("npc_ruins_dweller") { }
 
-	CreatureAI* GetAI(Creature* pCreature) const {
-		return new npc_ruins_dwellerAI(pCreature);
-	}
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_ruins_dwellerAI (creature);
+    }
 
-	struct npc_ruins_dwellerAI: public ScriptedAI {
-		npc_ruins_dwellerAI(Creature *c) :
-				ScriptedAI(c) {
-			pInstance = c->GetInstanceScript();
-		}
+    struct npc_ruins_dwellerAI : public ScriptedAI
+    {
+        npc_ruins_dwellerAI(Creature* c) : ScriptedAI(c)
+        {
+            pInstance = c->GetInstanceScript();
+        }
 
-		InstanceScript* pInstance;
+        uint32 uiSpringTimer;
+        uint32 uiRegurgitateTimer;
 
-		void JustDied(Unit * /*who*/) {
-			if (pInstance) {
-				pInstance->SetData64(DATA_RUIN_DWELLER_DIED, me->GetGUID());
-				if (pInstance->GetData(DATA_ALIVE_RUIN_DWELLERS) == 0)
-					me->SummonCreature(CREATURE_ECK, EckSpawnPoint,
-							TEMPSUMMON_CORPSE_TIMED_DESPAWN,
-							300 * IN_MILLISECONDS);
-			}
-		}
-	};
+        InstanceScript* pInstance;
+
+        void Reset()
+        {
+            uiRegurgitateTimer = urand(3*IN_MILLISECONDS, 6*IN_MILLISECONDS);
+            uiSpringTimer = urand(7*IN_MILLISECONDS, 10*IN_MILLISECONDS);
+        }
+
+        void EnterCombat(Unit* /*who*/) {}
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (uiRegurgitateTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_REGURGITATE);
+                uiRegurgitateTimer = urand(7*IN_MILLISECONDS, 18*IN_MILLISECONDS);
+            } else uiRegurgitateTimer -= diff;
+
+            if (uiSpringTimer <= diff)
+            {
+                Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1);
+                if (target && target->GetTypeId() == TYPEID_PLAYER)
+                    if ((me->GetExactDist(target->GetPositionX(), target->GetPositionY(), target->GetPositionZ()) > 5) && (me->GetExactDist(target->GetPositionX(), target->GetPositionY(), target->GetPositionZ()) < 30))
+                    {
+                        DoCast(target, SPELL_SPRING);
+                        uiSpringTimer = urand(12*IN_MILLISECONDS, 20*IN_MILLISECONDS);
+
+                        DoResetThreat();
+                        AttackStart(target);
+                        me->AddThreat(target, 1.0f);
+                    }
+            } else uiSpringTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+
+        void JustDied(Unit* /*who*/)
+        {
+            if (pInstance)
+            {
+                if (!me->FindNearestCreature(CREATURE_ECK, RANGE_ECK, true))
+            {
+                pInstance->SetData64(DATA_RUIN_DWELLER_DIED, me->GetGUID());
+                if (pInstance->GetData(DATA_ALIVE_RUIN_DWELLERS) == 0)
+                    me->SummonCreature(CREATURE_ECK, EckSpawnPoint, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 300*IN_MILLISECONDS);
+                }            
+            } 
+        }
+    };
+
 };
 
-void AddSC_boss_eck() {
-	new boss_eck();
-	new npc_ruins_dweller();
+void AddSC_boss_eck()
+{
+    new boss_eck();
+    new npc_ruins_dweller();
 }
--- src/server/scripts/Northrend/Gundrak/boss_moorabi.cpp
+++ src/server/scripts/Northrend/Gundrak/boss_moorabi.cpp
@@ -1,174 +1,212 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://www.getmangos.com/>
- *
- * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
- *
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
- *
  * Copyright (C) 2010-2011 ProjectSkyfire <http://www.projectskyfire.org/>
  * 
  * Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ScriptPCH.h"
 #include "gundrak.h"
 
-enum eSpells {
-	SPELL_DETERMINED_STAB = 55104,
-	SPELL_GROUND_TREMOR = 55142,
-	SPELL_NUMBING_SHOUT = 55106,
-	SPELL_DETERMINED_GORE = 55102,
-	H_SPELL_DETERMINED_GORE = 59444,
-	SPELL_QUAKE = 55101,
-	SPELL_NUMBING_ROAR = 55100,
-	SPELL_MOJO_FRENZY = 55163,
-	SPELL_TRANSFORMATION = 55098,
-//Periodic, The caster transforms into a powerful mammoth, increasing Physical damage done by 25% and granting immunity to Stun effects.
+enum Spells
+{
+    // troll form
+    SPELL_DETERMINED_STAB                         = 55104,
+    SPELL_GROUND_TREMOR                           = 55142,
+    SPELL_NUMBING_SHOUT                           = 55106,
+    SPELL_MOJO_FRENZY                             = 55163,
+    SPELL_MOJO_FRENZY_HASTE                       = 55096,
+    SPELL_TRANSFORMATION                          = 55098, //Periodic, The caster transforms into a powerful mammoth, increasing Physical damage done by 25% and granting immunity to Stun effects.
+    // mammoth
+    SPELL_DETERMINED_GORE                         = 55102,
+    H_SPELL_DETERMINED_GORE                       = 59444,
+    SPELL_QUAKE                                   = 55101,
+    SPELL_NUMBING_ROAR                            = 55100
 };
 
-enum eArchivements {
-	ACHIEV_LESS_RABI = 2040
+enum Says
+{
+    SAY_AGGRO                                     = -1604011,
+    SAY_QUAKE                                     = -1604012,
+    SAY_TRANSFORM                                 = -1604013,
+    SAY_SLAY_1                                    = -1604014,
+    SAY_SLAY_2                                    = -1604015,
+    SAY_SLAY_3                                    = -1604016,
+    SAY_DEATH                                     = -1604017,
+    EMOTE_TRANSFORM                               = -1604018,
+    EMOTE_TRANSFORMED          = -1604029
 };
 
-enum eSays {
-	SAY_AGGRO = -1604010,
-	//SAY_SLAY_1                                  = -1604011, // not in db
-	SAY_SLAY_2 = -1604012,
-	SAY_SLAY_3 = -1604013,
-	SAY_DEATH = -1604014,
-	SAY_TRANSFORM = -1604015,
-	SAY_QUAKE = -1604016,
-	EMOTE_TRANSFORM = -1604017
+enum Events
+{
+    EVENT_QUAKE = 1,
+    EVENT_SHOUT,
+    EVENT_STAB,
+    EVENT_TRANSFORM,
+    DATA_LESS_RABI
 };
 
-class boss_moorabi: public CreatureScript {
+class boss_moorabi : public CreatureScript
+{
 public:
-	boss_moorabi() :
-			CreatureScript("boss_moorabi") {
-	}
+        boss_moorabi() : CreatureScript("boss_moorabi")
+    {
+    }
 
-	CreatureAI* GetAI(Creature *pCreature) const {
-		return new boss_moorabiAI(pCreature);
-	}
+    struct boss_moorabiAI : public ScriptedAI
+    {
+        boss_moorabiAI(Creature* creature) : ScriptedAI(creature)
+        {
+                _instance = creature->GetInstanceScript();
+        
+            // make 55098 interruptable
+            SpellEntry* spell = (SpellEntry*)sSpellStore.LookupEntry(SPELL_TRANSFORMATION);
+            if (spell)
+                spell->InterruptFlags |= SPELL_INTERRUPT_FLAG_INTERRUPT;
+        }
 
-	struct boss_moorabiAI: public BossAI {
-		boss_moorabiAI(Creature* pCreature) :
-				BossAI(pCreature, DATA_MOORABI_EVENT) {
-			me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK,
-					true);
-			me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_GRIP, true);
-		}
+        void Reset()
+        {
+                _events.Reset();
+                _transformed = false;
 
-		bool bPhase;
+                if (_instance)
+                    _instance->SetData(DATA_MOORABI_EVENT, NOT_STARTED);
+        }
 
-		uint32 uiNumblingShoutTimer;
-		uint32 uiGroundTremorTimer;
-		uint32 uiDeterminedStabTimer;
-		uint32 uiTransformationTImer;
+        void EnterCombat(Unit* /*who*/)
+        {
+            DoScriptText(SAY_AGGRO, me);
+            DoCast(me, SPELL_MOJO_FRENZY, true);
 
-		void Reset() {
-			uiGroundTremorTimer = 18 * IN_MILLISECONDS;
-			uiNumblingShoutTimer = 10 * IN_MILLISECONDS;
-			uiDeterminedStabTimer = 20 * IN_MILLISECONDS;
-			uiTransformationTImer = 12 * IN_MILLISECONDS;
-			bPhase = false;
+                _events.ScheduleEvent(EVENT_QUAKE, 18*IN_MILLISECONDS);
+                _events.ScheduleEvent(EVENT_SHOUT, 15*IN_MILLISECONDS);
+                _events.ScheduleEvent(EVENT_STAB, 10*IN_MILLISECONDS);
+                _events.ScheduleEvent(EVENT_TRANSFORM, 12*IN_MILLISECONDS);
 
-			if (instance)
-				instance->SetData(DATA_MOORABI_EVENT, NOT_STARTED);
-		}
+                if (_instance)
+                    _instance->SetData(DATA_MOORABI_EVENT, IN_PROGRESS);
+         }
 
-		void EnterCombat(Unit* /*pWho*/) {
-			DoScriptText(SAY_AGGRO, me);
-			DoCast(me, SPELL_MOJO_FRENZY, true);
+        uint32 GetData(uint32 type)
+        {
+            if (type == DATA_LESS_RABI)
+                    return !_transformed ? 1 : 0;
 
-			if (instance)
-				instance->SetData(DATA_MOORABI_EVENT, IN_PROGRESS);
-		}
+            return 0;
+        }
 
-		void UpdateAI(const uint32 uiDiff) {
-			//Return since we have no target
-			if (!UpdateVictim())
-				return;
+         void JustDied(Unit* /*killer*/)
+         {
+            DoScriptText(SAY_DEATH, me);
 
-			if (!bPhase && me->HasAura(SPELL_TRANSFORMATION)) {
-				bPhase = true;
-				me->RemoveAura(SPELL_MOJO_FRENZY);
-			}
+                if (_instance)
+                    _instance->SetData(DATA_MOORABI_EVENT, DONE);
+        }
 
-			if (uiGroundTremorTimer <= uiDiff) {
-				DoScriptText(SAY_QUAKE, me);
-				if (bPhase)
-					DoCast(me->getVictim(), SPELL_QUAKE, true);
-				else
-					DoCast(me->getVictim(), SPELL_GROUND_TREMOR, true);
-				uiGroundTremorTimer = 10 * IN_MILLISECONDS;
-			} else
-				uiGroundTremorTimer -= uiDiff;
+        void KilledUnit(Unit* victim)
+        {
+            if (victim == me)
+                return;
 
-			if (uiNumblingShoutTimer <= uiDiff) {
-				if (bPhase)
-					DoCast(me->getVictim(), SPELL_NUMBING_ROAR, true);
-				else
-					DoCast(me->getVictim(), SPELL_NUMBING_SHOUT, true);
-				uiNumblingShoutTimer = 10 * IN_MILLISECONDS;
-			} else
-				uiNumblingShoutTimer -= uiDiff;
+            DoScriptText(RAND(SAY_SLAY_1,SAY_SLAY_2,SAY_SLAY_3), me);
+        }
 
-			if (uiDeterminedStabTimer <= uiDiff) {
-				if (bPhase)
-					DoCast(me->getVictim(), SPELL_DETERMINED_GORE);
-				else
-					DoCast(me->getVictim(), SPELL_DETERMINED_STAB, true);
-				uiDeterminedStabTimer = 8 * IN_MILLISECONDS;
-			} else
-				uiDeterminedStabTimer -= uiDiff;
+            void UpdateAI(uint32 const diff)
+            {
+                if (!UpdateVictim())
+                     return;
 
-			if (!bPhase && uiTransformationTImer <= uiDiff) {
-				DoScriptText(EMOTE_TRANSFORM, me);
-				DoScriptText(SAY_TRANSFORM, me);
-				DoCast(me, SPELL_TRANSFORMATION, false);
-				uiTransformationTImer = 10 * IN_MILLISECONDS;
-			} else
-				uiTransformationTImer -= uiDiff;
+                _events.Update(diff);
 
-			DoMeleeAttackIfReady();
-		}
+                if (me->HasUnitState(UNIT_STAT_CASTING))
+                    return;
 
-		void JustDied(Unit* /*pKiller*/) {
-			_JustDied();
-			DoScriptText(SAY_DEATH, me);
+                while (uint32 eventId = _events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                        case EVENT_QUAKE:
+                            DoScriptText(SAY_QUAKE, me);
+                            DoCastVictim(_transformed ? SPELL_GROUND_TREMOR : SPELL_QUAKE);
+                            _events.ScheduleEvent(EVENT_QUAKE, urand(10, 15) *IN_MILLISECONDS);
+                            break;
+                        case EVENT_SHOUT:
+                            DoCastVictim(_transformed ? SPELL_NUMBING_ROAR : SPELL_NUMBING_SHOUT);
+                            _events.ScheduleEvent(EVENT_SHOUT, 10*IN_MILLISECONDS);
+                            break;
+                        case EVENT_STAB:
+                            DoCastVictim(_transformed ? DUNGEON_MODE(SPELL_DETERMINED_GORE, H_SPELL_DETERMINED_GORE) : SPELL_DETERMINED_STAB);
+                            _events.ScheduleEvent(EVENT_STAB, 7*IN_MILLISECONDS);
+                            break;
+                        case EVENT_TRANSFORM:
+                            DoScriptText(EMOTE_TRANSFORM, me);
+                            DoScriptText(SAY_TRANSFORM, me);
+                            DoCast(me, SPELL_TRANSFORMATION);
+                            _events.ScheduleEvent(EVENT_TRANSFORM, 10*IN_MILLISECONDS);
+                            break;
+                    }
+                }
 
-			if (instance) {
-				instance->SetData(DATA_MOORABI_EVENT, DONE);
+                if (!_transformed && me->HasAura(SPELL_TRANSFORMATION))
+                {
+                    _transformed = true;
+                    _events.CancelEvent(EVENT_TRANSFORM);
+                    me->RemoveAura(SPELL_MOJO_FRENZY);
+                    DoScriptText(EMOTE_TRANSFORMED, me);
+                }
 
-				if (IsHeroic() && !bPhase)
-					instance->DoCompleteAchievement(ACHIEV_LESS_RABI);
-			}
-		}
+                DoMeleeAttackIfReady();
+             }
 
-		void KilledUnit(Unit* pVictim) {
-			if (pVictim == me)
-				return;
+        private:
+            InstanceScript* _instance;
+            EventMap _events;
+            bool _transformed;
+    };
 
-			DoScriptText(RAND(SAY_SLAY_2, SAY_SLAY_3), me);
-		}
-	};
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new boss_moorabiAI(creature);
+        }
 };
 
-void AddSC_boss_moorabi() {
-	new boss_moorabi();
+class achievement_less_rabi : public AchievementCriteriaScript
+{
+    public:
+        achievement_less_rabi() : AchievementCriteriaScript("achievement_less_rabi")
+        {
+        }
+
+        bool OnCheck(Player* /*player*/, Unit* target)
+        {
+            if (!target)
+                return false;
+
+            if (Creature* moorabi = target->ToCreature())
+                if (moorabi->AI()->GetData(DATA_LESS_RABI))
+                    return true;
+
+            return false;
+        }
+};
+
+void AddSC_boss_moorabi()
+{
+    new boss_moorabi();
+    new achievement_less_rabi();
 }
