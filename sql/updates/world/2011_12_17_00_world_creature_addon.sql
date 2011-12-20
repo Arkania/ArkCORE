166fcef250eff85d1d55e42f5e3b5ebe3a353bdb
misimouse <misimouse.dol@gmail.com>
2011-12-20 18:35:37 +0800

    DB/TYPO: Fix typo on Mikadmin commint. Addons still are in old format, please include a 0 (zero) after spell :=

--- sql/updates/world/2011_12_17_00_world_creature_addon.sql
+++ sql/updates/world/2011_12_17_00_world_creature_addon.sql
@@ -2,4 +2,4 @@
 -- By Mikadmin For Arkania
 
 DELETE FROM `creature_addon` WHERE (`guid`=305725);
-INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (305725, 0, 0, 65536, 1, 0, 79488);
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (305725, 0, 0, 65536, 1, 0, 79488 0);
