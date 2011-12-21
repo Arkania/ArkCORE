5a579944d3174c1cc484e0a327d9dbaea678c370
Spyder <serge@arkania.net>
2011-12-21 10:52:08 +0800

    Core/Scripts: Instance Uldaman updated.

--- src/server/scripts/EasternKingdoms/Uldaman/boss_ironaya.cpp
+++ src/server/scripts/EasternKingdoms/Uldaman/boss_ironaya.cpp
@@ -3,33 +3,29 @@
  *
  * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
+ * Copyright (C) 2010-2011 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
- * Copyright (C) 2010-2011 ProjectSkyfire <http://www.projectskyfire.org/>
- * 
- * Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* ScriptData
- SDName: Boss_Ironaya
- SD%Complete: 100
- SDComment:
- SDCategory: Uldaman
- EndScriptData */
+SDName: Boss_Ironaya
+SD%Complete: 100
+SDComment:
+SDCategory: Uldaman
+EndScriptData */
 
 #include "ScriptPCH.h"
 
@@ -39,77 +35,85 @@
 #define SPELL_KNOCKAWAY             10101
 #define SPELL_WSTOMP                11876
 
-class boss_ironaya: public CreatureScript {
-public:
+class boss_ironaya : public CreatureScript
+{
+    public:
 
-	boss_ironaya() :
-			CreatureScript("boss_ironaya") {
-	}
+        boss_ironaya()
+            : CreatureScript("boss_ironaya")
+        {
+        }
 
-	struct boss_ironayaAI: public ScriptedAI {
-		boss_ironayaAI(Creature* pCreature) :
-				ScriptedAI(pCreature) {
-		}
+        struct boss_ironayaAI : public ScriptedAI
+        {
+            boss_ironayaAI(Creature* creature) : ScriptedAI(creature) {}
 
-		uint32 uiArcingTimer;
-		bool bHasCastedWstomp;
-		bool bHasCastedKnockaway;
+            uint32 uiArcingTimer;
+            bool bHasCastedWstomp;
+            bool bHasCastedKnockaway;
 
-		void Reset() {
-			uiArcingTimer = 3000;
-			bHasCastedKnockaway = false;
-			bHasCastedWstomp = false;
-		}
+            void Reset()
+            {
+                uiArcingTimer = 3000;
+                bHasCastedKnockaway = false;
+                bHasCastedWstomp = false;
+            }
 
-		void EnterCombat(Unit * /*who*/) {
-			DoScriptText(SAY_AGGRO, me);
-		}
+            void EnterCombat(Unit* /*who*/)
+            {
+                DoScriptText(SAY_AGGRO, me);
+            }
 
-		void UpdateAI(const uint32 uiDiff) {
-			//Return since we have no target
-			if (!UpdateVictim())
-				return;
+            void UpdateAI(const uint32 uiDiff)
+            {
+                //Return since we have no target
+                if (!UpdateVictim())
+                    return;
 
-			//If we are <50% hp do knockaway ONCE
-			if (!bHasCastedKnockaway && HealthBelowPct(50)) {
-				DoCast(me->getVictim(), SPELL_KNOCKAWAY, true);
+                //If we are <50% hp do knockaway ONCE
+                if (!bHasCastedKnockaway && HealthBelowPct(50))
+                {
+                    DoCast(me->getVictim(), SPELL_KNOCKAWAY, true);
 
-				// current aggro target is knocked away pick new target
-				Unit* pTarget = SelectUnit(SELECT_TARGET_TOPAGGRO, 0);
+                    // current aggro target is knocked away pick new target
+                    Unit* target = SelectTarget(SELECT_TARGET_TOPAGGRO, 0);
 
-				if (!pTarget || pTarget == me->getVictim())
-					pTarget = SelectUnit(SELECT_TARGET_TOPAGGRO, 1);
+                    if (!target || target == me->getVictim())
+                        target = SelectTarget(SELECT_TARGET_TOPAGGRO, 1);
 
-				if (pTarget)
-					me->TauntApply(pTarget);
+                    if (target)
+                        me->TauntApply(target);
 
-				//Shouldn't cast this agian
-				bHasCastedKnockaway = true;
-			}
+                    //Shouldn't cast this agian
+                    bHasCastedKnockaway = true;
+                }
 
-			//uiArcingTimer
-			if (uiArcingTimer <= uiDiff) {
-				DoCast(me, SPELL_ARCINGSMASH);
-				uiArcingTimer = 13000;
-			} else
-				uiArcingTimer -= uiDiff;
+                //uiArcingTimer
+                if (uiArcingTimer <= uiDiff)
+                {
+                    DoCast(me, SPELL_ARCINGSMASH);
+                    uiArcingTimer = 13000;
+                } else uiArcingTimer -= uiDiff;
 
-			if (!bHasCastedWstomp && HealthBelowPct(25)) {
-				DoCast(me, SPELL_WSTOMP);
-				bHasCastedWstomp = true;
-			}
+                if (!bHasCastedWstomp && HealthBelowPct(25))
+                {
+                    DoCast(me, SPELL_WSTOMP);
+                    bHasCastedWstomp = true;
+                }
 
-			DoMeleeAttackIfReady();
-		}
-	};
+                DoMeleeAttackIfReady();
+            }
+        };
 
-	CreatureAI* GetAI(Creature* creature) const {
-		return new boss_ironayaAI(creature);
-	}
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new boss_ironayaAI(creature);
+        }
 };
 
 //This is the actual function called only once durring InitScripts()
 //It must define all handled functions that are to be run in this script
-void AddSC_boss_ironaya() {
-	new boss_ironaya();
+void AddSC_boss_ironaya()
+{
+    new boss_ironaya();
 }
