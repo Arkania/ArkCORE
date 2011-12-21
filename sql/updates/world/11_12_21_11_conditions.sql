5bc9ddf7d5ef19a8729af9a092dc102615637d22
redneo <alcoholaterapia@hotmail.com>
2011-12-22 00:36:34 +0800

    DB/CONDITIONS: Fix http://www.wowhead.com/item=52683 cant kill everybody

---
+++ sql/updates/world/11_12_21_11_conditions.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceID`=17 AND `SourceEntry`=74087 AND `SourceGroup`=0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ConditionTypeOrReference`,`elseGroup`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`Comment`) VALUES (17,0,74087,19,0,39619,0,0,0,'Blackjack only in Twilight Recruit');
+
