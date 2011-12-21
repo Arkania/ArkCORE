a8b8ee77b3455d8d4c915dc2fbda566ef74193a1
Spyder <serge@arkania.net>
2011-12-21 10:41:40 +0800

    Core/Scripts: Instance Utgarde Pinnacle updated.

--- src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/utgarde_pinnacle.h
+++ src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/utgarde_pinnacle.h
@@ -3,63 +3,50 @@
  *
  * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
+ * Copyright (C) 2010-2011 ArkCORE <http://www.arkania.net/>
  *
- * Copyright (C) 2010-2011 ProjectSkyfire <http://www.projectskyfire.org/>
- * 
- * Copyright (C) 2011 ArkCORE <http://www.arkania.net/>
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DEF_PINNACLE_H
 #define DEF_PINNACLE_H
 
-enum Data {
-	DATA_SVALA_SORROWGRAVE_EVENT,
-	DATA_GORTOK_PALEHOOF_EVENT,
-	DATA_SKADI_THE_RUTHLESS_EVENT,
-	DATA_KING_YMIRON_EVENT
+enum Data
+{
+    DATA_SVALA_SORROWGRAVE_EVENT,
+    DATA_GORTOK_PALEHOOF_EVENT,
+    DATA_SKADI_THE_RUTHLESS_EVENT,
+    DATA_KING_YMIRON_EVENT
 };
-enum Data64 {
-	DATA_SVALA,
-	DATA_SVALA_SORROWGRAVE,
-	DATA_GORTOK_PALEHOOF,
-	DATA_SKADI_THE_RUTHLESS,
-	DATA_MOB_GRAUF,
-	DATA_KING_YMIRON,
-	DATA_MOB_FRENZIED_WORGEN,
-	DATA_MOB_RAVENOUS_FURBOLG,
-	DATA_MOB_MASSIVE_JORMUNGAR,
-	DATA_MOB_FEROCIOUS_RHINO,
-	DATA_MOB_ORB,
-	DATA_GORTOK_PALEHOOF_SPHERE,
-	DATA_SACRIFICED_PLAYER
-};
-
-enum eCreatures {
-	BOSS_SVALA_SORROWGRAVE = 26668,
-	BOSS_GORTOK_PALEHOOF = 26687,
-	BOSS_SKADI_RUTHLESS = 26693,
-	BOSS_KING_YMIRON = 26861,
-	MOB_FRENZIED_WORGEN = 26683,
-	MOB_RAVENOUS_FURBOLG = 26684,
-	MOB_MASSIVE_JORMUNGAR = 26685,
-	MOB_FEROCIOUS_RHINO = 26686,
-	MOB_SVALA = 29281,
-	MOB_PALEHOOF_ORB = 26688,
+enum Data64
+{
+    DATA_SVALA,
+    DATA_SVALA_SORROWGRAVE,
+    DATA_GORTOK_PALEHOOF,
+    DATA_SKADI_THE_RUTHLESS,
+    DATA_GRAUF,
+    DATA_KING_YMIRON,
+    DATA_MOB_FRENZIED_WORGEN,
+    DATA_MOB_RAVENOUS_FURBOLG,
+    DATA_MOB_MASSIVE_JORMUNGAR,
+    DATA_MOB_FEROCIOUS_RHINO,
+    DATA_MOB_ORB,
+    DATA_GORTOK_PALEHOOF_SPHERE,
+    DATA_FLAME_BRAZIER_1,
+    DATA_FLAME_BRAZIER_2,
+    DATA_DOODAD_UTGARDE_MIRROR_FX01,
+    DATA_RITUAL_TARGET,
 };
 
 #endif
