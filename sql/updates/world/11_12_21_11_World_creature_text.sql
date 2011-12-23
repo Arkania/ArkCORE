e588fb831ec241d4e5d39df7788a6da323c9e3eb
misimouse <misimouse.dol@gmail.com>
2011-12-24 01:47:10 +0800

    DB/CREATURE TEXT: Typo type 15 is type 4 in old TC format

--- sql/updates/world/11_12_21_11_World_creature_text.sql
+++ sql/updates/world/11_12_21_11_World_creature_text.sql
@@ -6,7 +6,7 @@ UPDATE `creature_text` SET `type`='2' WHERE `type`='16'; -- Emote
 UPDATE `creature_text` SET `type`='1' WHERE `type`='14'; -- Yell
 UPDATE `creature_text` SET `type`='3' WHERE `type`='41'; -- Normal
 UPDATE `creature_text` SET `type`='5' WHERE `type`='42'; -- Boss Whisper 
-UPDATE `creature_text` SET `type`='5' WHERE `type`='15'; -- Whisper
+UPDATE `creature_text` SET `type`='4' WHERE `type`='15'; -- Whisper
 -- CREATURETEXT: Remove sound for unknow entrys
 UPDATE `creature_text` SET `sound`='0' WHERE `Entry` IN (23863);
 -- SCRIPTEXTS: change type 12 to 6
