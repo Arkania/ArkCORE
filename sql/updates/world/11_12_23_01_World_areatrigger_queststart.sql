f0c5b5486b39ae042dab23b68001c6911a42fa87
misimouse <misimouse.dol@gmail.com>
2011-12-23 19:11:20 +0800

    DB/AREATRIGGER QUESTSTART: Better fix with primary key and type fields

--- sql/updates/world/11_12_23_01_World_areatrigger_queststart.sql
+++ sql/updates/world/11_12_23_01_World_areatrigger_queststart.sql
@@ -3,6 +3,7 @@
 -- ERROR: [1054] Unknown column 'trigger_ID' in 'field list'
 DROP TABLE IF EXISTS `areatrigger_queststart`;
 CREATE TABLE `areatrigger_queststart` (
- `trigger_ID` INT ,
- `quest_ID` INT 
-);
+  `trigger_ID` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Identifier',
+  `quest_ID` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Quest Identifier',
+  PRIMARY KEY (`trigger_ID`)
+) ENGINE=MYISAM DEFAULT CHARSET=latin1;
