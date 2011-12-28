79bd98bcf7a566733efb4958038238eb45eae983
Mikadmin <mikadmin@wow-attitude.com>
2011-12-28 03:15:25 +0100

    DB/creature Celestine of the Harvest
    Fix id spawn http://www.wowhead.com/npc=44395 not http://www.wowhead.com/npc=44396

---
+++ sql/updates/world/11_12_28_02_World_creature.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature` WHERE `id`=44396;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(254377, 44395, 0, 1, 1, 33840, 44396, -8284.26, 724.349, 75.3668, 2.09439, 600, 0, 0, 1226, 1283, 0, 0, 0, 0, 0);
