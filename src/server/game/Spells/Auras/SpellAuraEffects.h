a72f885240597cdb35ebf9eadb7d3eaaf8298e9f
wlasser <lostbyte32@gmail.com>
2011-12-29 16:54:07 +0400

    Update src/server/game/Spells/Auras/SpellAuraEffects.h

--- src/server/game/Spells/Auras/SpellAuraEffects.h
+++ src/server/game/Spells/Auras/SpellAuraEffects.h
@@ -193,6 +193,8 @@ public:
 			bool apply) const;
 	void HandleModTrapLauncher(AuraApplication const * aurApp, uint8 mode,
 			bool apply) const;
+	void HandleAuraSaleForGuild(AuraApplication const * aurApp, uint8 mode,
+			bool apply) const;
 	void HandleModCamouflage(AuraApplication const * aurApp, uint8 mode,
 			bool apply) const;
 	void HandleModStealth(AuraApplication const * aurApp, uint8 mode,
