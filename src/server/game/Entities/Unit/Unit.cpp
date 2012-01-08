d40f1b94e7822e02f6eb3ddf3fa0af153952d1f6
Spyder <serge@arkania.net>
2012-01-08 14:21:35 -0600

    Core/Spell: Shooting Stars

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9045,51 +9045,52 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage,
 				}
 				break;
 			}
-			case SPELLFAMILY_DRUID:
-			{
-				// Druid Forms Trinket
-				if (auraSpellInfo->Id == 37336)
-				{
-					switch (GetShapeshiftForm())
-					{
-						case FORM_NONE:
-							trigger_spell_id = 37344;
-							break;
-						case FORM_CAT:
-							trigger_spell_id = 37341;
-							break;
-						case FORM_BEAR:
-						case FORM_DIREBEAR:
-							trigger_spell_id = 37340;
-							break;
-						case FORM_TREE:
-							trigger_spell_id = 37342;
-							break;
-						case FORM_MOONKIN:
-							trigger_spell_id = 37343;
-							break;
-						default:
-							return false;
-					}
-				}
-				// Druid T9 Feral Relic (Lacerate, Swipe, Mangle, and Shred)
-				else if (auraSpellInfo->Id == 67353)
-				{
-					switch (GetShapeshiftForm())
-					{
-						case FORM_CAT:
-							trigger_spell_id = 67355;
-							break;
-						case FORM_BEAR:
-						case FORM_DIREBEAR:
-							trigger_spell_id = 67354;
-							break;
-						default:
-							return false;
-					}
-				}
-				break;
-			}
+            case SPELLFAMILY_DRUID:
+            {
+                switch (auraSpellInfo->Id)
+                {
+                    // Druid Forms Trinket
+                    case 37336:
+                    {
+                        switch (GetShapeshiftForm())
+                        {
+                            case FORM_NONE:     trigger_spell_id = 37344; break;
+                            case FORM_CAT:      trigger_spell_id = 37341; break;
+                            case FORM_BEAR:
+                            case FORM_DIREBEAR: trigger_spell_id = 37340; break;
+                            case FORM_TREE:     trigger_spell_id = 37342; break;
+                            case FORM_MOONKIN:  trigger_spell_id = 37343; break;
+                            default:
+                                return false;
+                        }
+                        break;
+                    }
+                    // Druid T9 Feral Relic (Lacerate, Swipe, Mangle, and Shred)
+                    case 67353:
+                    {
+                        switch (GetShapeshiftForm())
+                        {
+                            case FORM_CAT:      trigger_spell_id = 67355; break;
+                            case FORM_BEAR:
+                            case FORM_DIREBEAR: trigger_spell_id = 67354; break;
+                            default:
+                                return false;
+                        }
+                        break;
+                    }
+                    // Shooting Stars
+                    case 93398: // Rank 1
+                    case 93399: // Rank 2
+                    {
+                        if (GetTypeId() == TYPEID_PLAYER)
+                            ToPlayer()->RemoveSpellCooldown(78674, true); // Remove cooldown of Starsurge
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                break;
+            }
 			case SPELLFAMILY_HUNTER:
 			{
 				if (auraSpellInfo->SpellIconID == 3247) // Piercing Shots 1, 2, 3
