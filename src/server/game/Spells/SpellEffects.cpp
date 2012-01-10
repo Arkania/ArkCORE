6277efff6cb5adfe8b7a718bf376e81609fa9a6c
dimiandre <dimiandre@gmail.com>
2012-01-10 21:32:50 +0100

    Little typo

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -6970,7 +6970,7 @@ void Spell::EffectScriptEffect(SpellEffIndex effIndex)
         {
             if (m_spellInfo->Id == 87151)
             {
-                if (m_caster->HasAura(81661)||m_caster->HasAura(81660))
+                if (m_caster->HasAura(81661)|| m_caster->HasAura(87118))
                 {
                     if (Aura* evangelism = m_caster->GetAura(81661))
                     {
