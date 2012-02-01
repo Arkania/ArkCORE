f632eee636beceeaf7b78c680869fededb2dff08
Walkum <juanmavictoria@gmail.com>
2012-02-01 23:04:19 +0100

    DB/Items: Fix typos on last commits.

--- sql/updates/world/12_02_01_02_disenchant_loot_template.sql
+++ sql/updates/world/12_02_01_02_disenchant_loot_template.sql
@@ -1,14 +1,14 @@
-- item 60211
+-- item 60211
 DELETE FROM `disenchant_loot_template` WHERE (`entry`=60211);
 INSERT INTO `disenchant_loot_template` VALUES 
 (60211, 52722, 100, 1, 0, 1, 2);
 
-- item 59420
+-- item 59420
 DELETE FROM `disenchant_loot_template` WHERE (`entry`=59420);
 INSERT INTO `disenchant_loot_template` VALUES 
 (59420, 52722, 100, 1, 0, 1, 2);
 
-- item 60202
+-- item 60202
 DELETE FROM `disenchant_loot_template` WHERE (`entry`=60202);
 INSERT INTO `disenchant_loot_template` VALUES 
 (60202, 52722, 100, 1, 0, 1, 3);
