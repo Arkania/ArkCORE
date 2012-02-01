f99d7b1b431dd2cc83cb1a9ad3db763ff6dc72f6
Mikadmin <mikadmin@wow-attitude.com>
2012-02-01 18:09:51 +0100

    DB/ITEM Tsanga's Helm
    Fix disenchante item

--- sql/updates/world/12_02_01_02_disenchant_loot_template.sql
+++ sql/updates/world/12_02_01_02_disenchant_loot_template.sql
@@ -7,3 +7,8 @@ INSERT INTO `disenchant_loot_template` VALUES
 DELETE FROM `disenchant_loot_template` WHERE (`entry`=59420);
 INSERT INTO `disenchant_loot_template` VALUES 
 (59420, 52722, 100, 1, 0, 1, 2);
+
+- item 60202
+DELETE FROM `disenchant_loot_template` WHERE (`entry`=60202);
+INSERT INTO `disenchant_loot_template` VALUES 
+(60202, 52722, 100, 1, 0, 1, 3);
