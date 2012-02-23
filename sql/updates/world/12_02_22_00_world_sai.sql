1889792664f980bc601c3043304b6cb3d0b6801f
Mikadmin <mikadmin@wow-attitude.com>
2012-02-23 12:22:39 +0100

    DB/SAI SMALL FIX

--- sql/updates/world/12_02_22_00_world_sai.sql
+++ sql/updates/world/12_02_22_00_world_sai.sql
@@ -418,7 +418,7 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,4,0,100,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
 (@ENTRY,0,1,2,6,0,100,1,0,0,0,0,11,20818,3,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Summon Foulweald Totem Basket on Death'),
-(@ENTRY,0,2,0,61,0,100,1,0,0,0,0,1,1,3,0,0,0,0,1,0,0,0,0,0,0,0,'Text on Death'),
+(@ENTRY,0,2,0,61,0,100,1,0,0,0,0,1,1,3,0,0,0,0,1,0,0,0,0,0,0,0,'Text on Death');
 -- NPC talk text insert
 SET @ENTRY := 12918;
 DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
@@ -1634,7 +1634,7 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,10,0,0,1,100,0,8000,9000,24000,28000,11,15798,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Moonfire');
 
 
-301 - 400 of 415Next 
+-- 301 - 400 of 415Next 
 
 
 
