2112673cd404a04043bdb03524a86ebc77cfbabc
adrianrp1988 <arodriguez@estudiantes.uci.cu>
2012-02-24 02:25:35 -0500

    Core/Spells Fire Mage talent Combustion Fix

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -6978,6 +6978,21 @@ void Spell::EffectScriptEffect(SpellEffIndex effIndex)
             }
             break;
         }
+          case SPELLFAMILY_MAGE:
+        {
+            if (m_spellInfo->Id == 11129) //Combustion
+            {
+                //I assume initial periodic damage is 0 if no Dots on target
+                int32 bp = 0;
+                Unit::AuraEffectList const &mPeriodic =    unitTarget->GetAuraEffectsByType(SPELL_AURA_PERIODIC_DAMAGE);
+                //Cycle trough all periodic auras to increase Combustion periodic damage
+                for (Unit::AuraEffectList::const_iterator i = mPeriodic.begin(); i != mPeriodic.end(); ++i)                 
+                    if ((*i)->GetCasterGUID() == m_caster->GetGUID())
+                        bp += (*i)->GetAmount();             
+                m_caster->CastCustomSpell(unitTarget, 83853, &bp,NULL, NULL, true);
+            }
+            break;
+        }
         case SPELLFAMILY_PRIEST:
         {
             if (m_spellInfo->Id == 87151)
