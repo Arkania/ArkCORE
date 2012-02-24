8f1bb837d2104384da4a2daa99e29b3133a17dae
Mikadmin <mikadmin@wow-attitude.com>
2012-02-24 11:34:39 +0100

    DB/SPELL Fix Dark Intent
    Thank Keital http://forums.arkania.net/threads/warlock-dark-intent-fix.4741/

---
+++ sql/updates/world/12_02_24_spell_linked_spell.sql
@@ -0,0 +1,5 @@
+-- Fix Dark Intent
+-- By Keital for Arkania
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES 
+('85767', '85768', '0', 'Cast Dark Intent On Self');
