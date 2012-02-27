9af60bc799373feb5468187e4d273f8250a0df61
Mikadmin <mikadmin@wow-attitude.com>
2012-02-27 09:04:24 +0100

    DB/CONDITIONS  Billy Goat Blaster
    Fix usebugg item
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_02_27_conditions.sql
@@ -0,0 +1,6 @@
+-- Billy Goat Blaster
+-- By Mikadmin For Arkania
+-- Fix condition
+
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(17, 0, 86425, 0, 29, 46393, 5, 0, 0, '', NULL);
