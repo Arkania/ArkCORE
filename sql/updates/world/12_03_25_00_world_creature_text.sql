f5071291cb7d2499b30d6c224e416cec9e0abb5c
cooler-SAI <cooler19860212@yahoo.com>
2012-03-25 21:22:41 +0400

    DB/Creature text: Hot fix for all my past scripts texts ( moved to new standart creature_text typos )

---
+++ sql/updates/world/12_03_25_00_world_creature_text.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_text` SET `type`='12' WHERE `type`='0';
+UPDATE `creature_text` SET `type`='14' WHERE `type`='1';
+UPDATE `creature_text` SET `type`='16' WHERE `type`='2';
+UPDATE `creature_text` SET `type`='41' WHERE `type`='6';
