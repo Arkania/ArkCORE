220cd2a5fe14da24a08176fbd44e5a969bb0d449
Mikadmin <mikadmin@wow-attitude.com>
2012-04-22 15:49:21 +0200

    add fix health

--- sql/updates/world/12_04_22_creature_template.sql
+++ sql/updates/world/12_04_22_creature_template.sql
@@ -2,7 +2,9 @@
 -- By mikadmin for Arknania
 -- Fix Focus
 
-UPDATE `creature_template` SET `InhabitType` = '2' WHERE `creature_template`.`entry` =46868;
+UPDATE `creature_template` SET `InhabitType` = '2',
+`exp` = '3',
+`Health_mod` = '0.8323' WHERE `creature_template`.`entry` =46868;
 
 DELETE FROM `skinning_loot_template` WHERE (`entry`=46868);
 INSERT INTO `skinning_loot_template` VALUES 
