3b3a7130f3d8fc089e06cc4bc05ff442c531dfd7
Mikadmin <mikadmin@wow-attitude.com>
2012-04-25 11:45:52 +0200

    DB/CREATURE Cho'gall
    Fix Damage
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_04_25_creature_template_09.sql
@@ -0,0 +1,27 @@
+-- ChoGall
+-- By Mikadmin for Arkania
+-- Fix Damage
+
+UPDATE `creature_template` SET `mindmg` = '1225.5',
+`maxdmg` = '1544.7',
+`attackpower` = '1651',
+`dmg_multiplier` = '7.5',
+`baseattacktime` = '2000' WHERE `creature_template`.`entry` =43324;
+
+UPDATE `creature_template` SET `mindmg` = '1225.5',
+`maxdmg` = '1544.7',
+`attackpower` = '1651',
+`dmg_multiplier` = '35',
+`baseattacktime` = '2000' WHERE `creature_template`.`entry` =50131;
+
+UPDATE `creature_template` SET `mindmg` = '1225.5',
+`maxdmg` = '1544.7',
+`attackpower` = '1651',
+`dmg_multiplier` = '15',
+`baseattacktime` = '2000' WHERE `creature_template`.`entry` =50132;
+
+UPDATE `creature_template` SET `mindmg` = '1225.5',
+`maxdmg` = '1544.7',
+`attackpower` = '1651',
+`dmg_multiplier` = '70',
+`baseattacktime` = '2000' WHERE `creature_template`.`entry` =50133;
