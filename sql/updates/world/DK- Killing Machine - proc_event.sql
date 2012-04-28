09f8122df2e92d4f146c77433069dd8aa616c952
Kraven5 <git@github.com:Arkania/ArkCORE.git>
2012-04-28 07:06:12 -0300

    Killing Machine Process

---
+++ sql/updates/world/DK- Killing Machine - proc_event.sql
@@ -0,0 +1,12 @@
+-- Killing Machine (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (51123);
+INSERT INTO `spell_proc_event` VALUES (51123, 0x01, 0x0F, 0x00000000, 0x00000000, 0x00000000, 0x00000004, 0x00000000, 0, 100, 0);
+
+-- Killing Machine (Rank 2)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (51127);
+INSERT INTO `spell_proc_event` VALUES (51127, 0x01, 0x0F, 0x00000000, 0x00000000, 0x00000000, 0x00000004, 0x00000000, 0, 100, 0);
+
+-- Killing Machine (Rank 3)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (51128);
+INSERT INTO `spell_proc_event` VALUES (51128, 0x01, 0x0F, 0x00000000, 0x00000000, 0x00000000, 0x00000004, 0x00000000, 0, 100, 0);
+
