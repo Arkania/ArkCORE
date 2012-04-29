0a9de74269740c67282f32d41324ed21eea48c2d
Spyder <serge@arkania.net>
2012-04-29 16:31:36 -0400

    DB/Misc: Clean also old currencies from item_instance.

--- sql/convert_trinity_335_to_arkcore/characters_TrinityCore3.3.5a_to_ArkCORE4.0.6a.sql
+++ sql/convert_trinity_335_to_arkcore/characters_TrinityCore3.3.5a_to_ArkCORE4.0.6a.sql
@@ -6,6 +6,23 @@
 
 -- Clean old currencies, this broken pets bar
 DELETE FROM `character_inventory` WHERE `slot` IN ('118','135');
+DELETE FROM `item_instance` WHERE `itemEntry` IN (
+49426, -- Emblem of Frost
+47241, -- Emblem of Triumph
+40752, -- Emblem of Heroism
+40753, -- Emblem of Valor
+45624, -- Emblem of Conquest
+20559, -- Arathi Basin Mark of Honor
+29024, -- Eye of the Storm Mark of Honor
+20560, -- Alterac Valley Mark of Honor
+20558, -- Warsong Gulch Mark of Honor
+43228, -- Stone Keeper's Shard	
+42425, -- Strand of the Ancients Mark of Honor
+47395, -- Isle of Conquest Mark of Honor
+43308, -- Old Honor Points
+43307  -- Old Arena Points 
+);
+
 
 -- account_data
 ALTER TABLE `account_data` 
