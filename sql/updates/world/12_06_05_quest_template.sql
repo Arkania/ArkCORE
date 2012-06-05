2381106fc12d9423c548105c57b00c6c23d614a6
Nanaki <nanaki529@gmail.com>
2012-06-05 21:48:20 +0200

    Fix RequiredSkillValue for a bunch of quests.

---
+++ sql/updates/world/12_06_05_quest_template.sql
@@ -0,0 +1,103 @@
+-- Fix RequiredSkillValue for a bunch of quests.
+-- By Nanaki for Arkania.
+
+-- Quests that require a certain profession skill value.
+UPDATE `quest_template` SET `RequiredSkillValue`='50' WHERE `entry`='866';    -- Root Samples
+UPDATE `quest_template` SET `RequiredSkillValue`='40' WHERE `entry`='1578';   -- Supplying the Front
+UPDATE `quest_template` SET `RequiredSkillValue`='20' WHERE `entry`='1581';   -- Elixirs for the Bladeleafs
+UPDATE `quest_template` SET `RequiredSkillValue`='70' WHERE `entry`='1582';   -- Moonglow Vest
+UPDATE `quest_template` SET `RequiredSkillValue`='60' WHERE `entry`='1618';   -- Gearing Redridge
+UPDATE `quest_template` SET `RequiredSkillValue`='140' WHERE `entry`='2751';  -- Barbaric Battlements
+UPDATE `quest_template` SET `RequiredSkillValue`='135' WHERE `entry`='2752';  -- On Iron Pauldrons
+UPDATE `quest_template` SET `RequiredSkillValue`='170' WHERE `entry`='2753';  -- Trampled Under Foot
+UPDATE `quest_template` SET `RequiredSkillValue`='155' WHERE `entry`='2754';  -- Horns of Frenzy
+UPDATE `quest_template` SET `RequiredSkillValue`='215' WHERE `entry`='2755';  -- Joys of Omosh
+UPDATE `quest_template` SET `RequiredSkillValue`='200' WHERE `entry`='3630';  -- Gnome Engineering
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6607';  -- Nat Pagle, Angler Extreme
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6608';  -- You Too Good.
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6609';  -- I Got Nothin' Left!
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6610';  -- Clamlette Surprise
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6611';  -- To Gadgetzan You Go!
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6612';  -- I Know A Guy...
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6622';  -- Triage
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6623';  -- Horde Trauma
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6624';  -- Triage
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='6625';  -- Alliance Trauma
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='8193';    -- Master Angler
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='8194';    -- Apprentice Angler
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='8221';    -- Rare Fish - Keefer's Angelfish
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='8224';    -- Rare Fish - Dezian Queenfish
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='8225';    -- Rare Fish - Brownell's Blue Striped Racer
+UPDATE `quest_template` SET `RequiredSkillValue`='305' WHERE `entry`='9635';  -- The Zapthrottle Mote Extractor!
+UPDATE `quest_template` SET `RequiredSkillValue`='305' WHERE `entry`='9636';  -- The Zapthrottle Mote Extractor!
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11377'; -- Revenge is Tasty
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11379'; -- Super Hot Stew
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11380'; -- Manalicious
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11381'; -- Soup for the Soul
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11531'; -- Strange Engine Part
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11665'; -- Crocolisks in the City
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11666'; -- Bait Bandits
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11667'; -- The One That Got Away
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11668'; -- Shrimpin' Ain't Easy
+UPDATE `quest_template` SET `RequiredSkillValue`='275' WHERE `entry`='11669'; -- Felblood Fillet
+UPDATE `quest_template` SET `RequiredSkillValue`='350' WHERE `entry`='13087'; -- Northern Cooking
+UPDATE `quest_template` SET `RequiredSkillValue`='350' WHERE `entry`='13088'; -- Northern Cooking
+UPDATE `quest_template` SET `RequiredSkillValue`='350' WHERE `entry`='13089'; -- Northern Cooking
+UPDATE `quest_template` SET `RequiredSkillValue`='350' WHERE `entry`='13090'; -- Northern Cooking
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13100'; -- Infused Mushroom Meatloaf
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13101'; -- Convention at the Legerdemain
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13102'; -- Sewer Stew
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13103'; -- Cheese for Glowergold
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13107'; -- Mustard Dogs!
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13112'; -- Infused Mushroom Meatloaf
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13113'; -- Convention at the Legerdemain
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13114'; -- Sewer Stew
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13115'; -- Cheese for Glowergold
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='13116'; -- Mustard Dogs!
+UPDATE `quest_template` SET `RequiredSkillValue`='375' WHERE `entry`='13148'; -- Necklace Repair
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='13825'; -- Clamlette Surprise
+UPDATE `quest_template` SET `RequiredSkillValue`='225' WHERE `entry`='13826'; -- Nat Pagle, Angler Extreme
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='13830';   -- The Ghostfish
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='13832';   -- Jewel Of The Sewers
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='13833';   -- Blood Is Thicker
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='13834';   -- Dangerously Delicious
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='13836';   -- Disarmed!
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='24803';   -- Kalu'ak Fishing Derby
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='24806';   -- Better Luck Next Time
+UPDATE `quest_template` SET `RequiredSkillValue`='475' WHERE `entry`='25047'; -- Broken Panther Figurine
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26153';   -- Penny's Pumpkin Pancakes
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26177';   -- Feeling Crabby?
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26183';   -- The King's Cider
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26190';   -- A Fisherman's Feast
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26192';   -- Orphans Like Cookies Too!
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26220';   -- Everything Is Better with Bacon
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26226';   -- Crawfish Creole
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26227';   -- Careful, This Fruit Bites Back
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26233';   -- Stealing From Our Own
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26234';   -- Stealing From Our Own
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26235';   -- Even Thieves Get Hungry
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26414';   -- Hitting a Walleye
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26420';   -- Diggin' For Worms
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26442';   -- Rock Lobster
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26488';   -- Big Gulp
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26536';   -- Thunder Falls
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26543';   -- Clammy Hands
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26556';   -- No Dumping Allowed
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26557';   -- A Staggering Effort
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26572';   -- A Golden Opportunity
+UPDATE `quest_template` SET `RequiredSkillValue`='1' WHERE `entry`='26588';   -- A Furious Catch
+UPDATE `quest_template` SET `RequiredSkillValue`='475' WHERE `entry`='28775'; -- Broken Serpent Figurine
+UPDATE `quest_template` SET `RequiredSkillValue`='475' WHERE `entry`='28776'; -- Broken Earthen Figurine
+UPDATE `quest_template` SET `RequiredSkillValue`='475' WHERE `entry`='28777'; -- Broken Owl Figurine
+UPDATE `quest_template` SET `RequiredSkillValue`='475' WHERE `entry`='28778'; -- Broken Boar Figurine
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28798'; -- Waters of Elune
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28799'; -- Might of the Earthen
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28800'; -- Whispers of the Djinn
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28801'; -- Tol'vir Heiroglyphics
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28802'; -- Map of the Architects
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28803'; -- Vengeance of the Wildhammer
+UPDATE `quest_template` SET `RequiredSkillValue`='425' WHERE `entry`='28804'; -- Dark Iron Contingency Plan
+
+-- These quests do not require any profession.
+UPDATE `quest_template` SET `SkillOrClassMask`='0' WHERE `entry` IN ('12517', '12518', '12798', '13311', '13479', '13480', '13483', '13484', '13502', '13503');
+
