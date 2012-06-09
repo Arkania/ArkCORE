516012c9fb90b7569173e7f83a8db8d0c3565f9a
Bootz <bootz@projectskyfire.org>
2012-06-09 16:50:43 -0500

    DB: Fixing some startup errors. * Fixed console errors for arena season 9 * fixed console errors for outdoor_pvp (WG) * fixed spell_bonus data that was not matching dbc's
    Signed-off-by: Bootz <bootz@projectskyfire.org>

---
+++ sql/updates/world/12_06_09_04_world_spell_bonus_data.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_bonus_data` SET `direct_bonus`= 0.536 WHERE `entry`= 18562; -- Name: Swiftmend
+UPDATE `spell_bonus_data` SET `direct_bonus`= 0.076 WHERE `entry`= 73921; -- Name: Healing Rain
+UPDATE `spell_bonus_data` SET `direct_bonus`= 0.6032 WHERE `entry`= 78777; -- Name: Wild Mushroom
