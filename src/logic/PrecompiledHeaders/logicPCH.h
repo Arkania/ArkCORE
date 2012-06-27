aba06702cad19697b3ac2dc21e28344023252eea
Spyder <serge@arkania.net>
2012-06-26 22:59:27 -0600

    Core/Misc: First step, update new files tree structure.

---
+++ src/logic/PrecompiledHeaders/logicPCH.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2005 - 2012 MaNGOS <http://www.getmangos.com/>
+ *
+ * Copyright (C) 2008 - 2012 Trinity <http://www.trinitycore.org/>
+ *
+ * Copyright (C) 2010 - 2012 ProjectSkyfire <http://www.projectskyfire.org/>
+ *
+ * Copyright (C) 2011 - 2012 ArkCORE <http://www.arkania.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+//add here most rarely modified headers to speed up debug build compilation
+#ifndef _LOGICPCH_H
+#define _LOGICPCH_H
+#ifdef ARKCORE_COREPCH
+#include "WorldSocket.h"        // must be first to make ACE happy with ACE includes in it
+#include "Common.h"
+
+#include "MapManager.h"
+#include "Log.h"
+#include "ObjectAccessor.h"
+#include "ObjectDefines.h"
+#include "SQLStorage.h"
+#include "Opcodes.h"
+#include "SharedDefines.h"
+#include "ObjectMgr.h"
+#include "Util.h"
+
+#endif /* ARKCORE_COREPCH */
+#endif  /* _LOGICPCH_H */
