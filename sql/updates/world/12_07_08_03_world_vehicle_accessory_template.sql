f79e7a0b32edc7426d2cf79e56139218ea2738a5
Mikadmin <mikadmin@wow-attitude.com>
2012-07-10 14:34:57 +0200

    fix typo
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

--- sql/updates/world/12_07_08_03_world_vehicle_accessory_template.sql
+++ sql/updates/world/12_07_08_03_world_vehicle_accessory_template.sql
@@ -2,4 +2,4 @@ RENAME TABLE `vehicle_accessory` TO `vehicle_template_accessory`;
 
 ALTER TABLE `vehicle_template_accessory` 
 ADD COLUMN  `summontype` TINYINT(3) UNSIGNED NOT NULL DEFAULT 6 COMMENT 'see enum TempSummonType' AFTER `description`,
-ADD COLUMN  `summontimer` INT(10) UNSIGNED NOT NULL DEFAULT 30000 COMMENT 'timer, only relevant for certain summontypes' AFTER `summontype``;
+ADD COLUMN  `summontimer` INT(10) UNSIGNED NOT NULL DEFAULT 30000 COMMENT 'timer, only relevant for certain summontypes' AFTER `summontype`;
