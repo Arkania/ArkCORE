8a490e259791816835a012d66ecaf6a0c0e4faf7
Mikadmin <mikadmin@wow-attitude.com>
2012-07-12 16:49:50 +0200

    DB/gameobject_template fix error consol
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_07_12_02_world_gameobject_template.sql
@@ -0,0 +1,8 @@
+-- gameobject_template
+-- By Mikadmin For ARKania
+-- Fix error consol
+
+UPDATE `gameobject_template` SET `data1` = '202560' WHERE `entry` =202560;
+UPDATE `gameobject_template` SET `data1` = '203015' WHERE `entry` =203015;
+UPDATE `gameobject_template` SET `data1` = '203016' WHERE `entry` =203016;
+UPDATE `gameobject_template` SET `data1` = '203061' WHERE `entry` =203061;
