5bde5ce8d8312afc1d8c9a9d32bcceb2dc5577cd
Mikadmin <mikadmin@wow-attitude.com>
2012-07-12 16:53:22 +0200

    Oups !!

--- sql/updates/world/12_07_12_02_world_gameobject_template.sql
+++ sql/updates/world/12_07_12_02_world_gameobject_template.sql
@@ -2,6 +2,7 @@
 -- By Mikadmin For ARKania
 -- Fix error consol
 
+UPDATE `gameobject_template` SET `data1` = '113768' WHERE `entry` =113768;
 UPDATE `gameobject_template` SET `data1` = '202560' WHERE `entry` =202560;
 UPDATE `gameobject_template` SET `data1` = '203015' WHERE `entry` =203015;
 UPDATE `gameobject_template` SET `data1` = '203016' WHERE `entry` =203016;
