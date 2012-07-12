401b737e4c9b181432f61269d69ea40606fb6b52
Mikadmin <mikadmin@wow-attitude.com>
2012-07-12 15:07:54 +0200

    DB/quest_template add new fix error consol.
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

--- sql/updates/world/12_07_12_01_world_quest_template.sql
+++ sql/updates/world/12_07_12_01_world_quest_template.sql
@@ -4,4 +4,4 @@
 
 UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 0 WHERE `entry` = 14293;
 UPDATE `quest_template` SET `ReqSpellCast1` = 0 WHERE `entry` = 14276;
-
+UPDATE `quest_template` SET `SpecialFlags` = 2 WHERE `entry` = 14154;
