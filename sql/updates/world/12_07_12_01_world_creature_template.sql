1f4ee189e6d5e71133529afffb3fbdba59ac7710
Mikadmin <mikadmin@wow-attitude.com>
2012-07-12 15:57:58 +0200

    DB/creature_template
    Fix error consol
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_07_12_01_world_creature_template.sql
@@ -0,0 +1,6 @@
+-- Fix error consol
+-- By mikadmin For ARKania
+
+UPDATE `creature_template` SET `lootid` = '0' WHERE `entry` = 42835;
+UPDATE `creature_template` SET `lootid` = '0' WHERE `entry` = 44172;
+UPDATE `creature_template` SET `lootid` = '0' WHERE `entry` = 44206;
