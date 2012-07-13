0ae4f4091a80e1fce82e4538e9b50d66b8164216
Mikadmin <mikadmin@wow-attitude.com>
2012-07-13 12:02:51 +0200

    DB/reference_loot_template Fix error consol
    Deleted because already use in creature_loot_template
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_07_13_00_world_reference_loot_template.sql
@@ -0,0 +1,5 @@
+-- Fix error consol: reference_loot_template
+-- By Mikadmin For ARKania
+-- Deleted because already use in creature_loot_template
+
+DELETE FROM `reference_loot_template` WHERE `entry` = 43296;
