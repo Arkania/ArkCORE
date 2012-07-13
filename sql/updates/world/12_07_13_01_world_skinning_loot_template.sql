2faf8202fcf26bebb00b7a89d05ec608aee7013f
Mikadmin <mikadmin@wow-attitude.com>
2012-07-13 11:47:23 +0200

    DB/skinning_loot_template Fix error consol: Wrong skinning_loot
    Deleted because entry add in patch 4.2
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_07_13_01_world_skinning_loot_template.sql
@@ -0,0 +1,10 @@
+-- Fix error consol: Wrong skinning_loot
+-- By Mikadmin For ARKania
+-- Deleted because entry add in patch 4.2
+
+DELETE FROM `skinning_loot_template` WHERE `entry` = 52498;
+DELETE FROM `skinning_loot_template` WHERE `entry` = 53095; 
+DELETE FROM `skinning_loot_template` WHERE `entry` = 53128; 
+DELETE FROM `skinning_loot_template` WHERE `entry` = 53545; 
+DELETE FROM `skinning_loot_template` WHERE `entry` = 53617; 
+DELETE FROM `skinning_loot_template` WHERE `entry` = 53631; 
