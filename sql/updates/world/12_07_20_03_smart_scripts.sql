195b9855264aded0b38fa5e744c083844fd77c66
cooler-SAI <cooler19860212@yahoo.com>
2012-07-21 21:33:10 +0400

    typo fix, thanks to @Zeuh

--- sql/updates/world/12_07_20_03_smart_scripts.sql
+++ sql/updates/world/12_07_20_03_smart_scripts.sql
@@ -556,7 +556,7 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,4000,5000,13000,17000,11,82376,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Frost Smash'),
-(@ENTRY,0,0,0,9,0,100,0,0,8,18000,21000,11,10987,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Geyser on Close');
+(@ENTRY,0,1,0,9,0,100,0,0,8,18000,21000,11,10987,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Geyser on Close');
 
 -- Twisted Reflection of Narain
 SET @ENTRY := 44722;
