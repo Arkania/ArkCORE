c7efc2fe5323329fe71a23a880ac95789a1c72a5
cooler-SAI <cooler19860212@yahoo.com>
2012-07-30 14:21:37 +0400

    DB/SAI: added Gruuls' Lair Raid scripts

---
+++ sql/updates/world/12_07_30_00_smart_scripts.sql
@@ -0,0 +1,29 @@
+ 
+  /* Raid Normal for 25 people */
+ /* Trash Mobs */
+ 
+-- Gronn-Priest
+SET @ENTRY := 21350;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,9,0,100,2,0,8,12000,19000,11,22884,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Psychic Scream on Close'),
+(@ENTRY,0,1,0,0,0,100,2,7000,16000,21000,27000,11,36679,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Renew'),
+(@ENTRY,0,2,0,14,0,100,2,140000,40,20000,25000,11,36678,0,0,0,0,0,7,0,0,0,0,0,0,0,'Cast Heal on Friendlies');
+
+-- Lair Brute
+SET @ENTRY := 19389;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,9,0,100,2,0,5,8500,22000,11,58460,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Brutal Strike on Close'),
+(@ENTRY,0,1,0,9,0,100,2,0,5,7000,16000,11,39174,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Cleave on Close'),
+(@ENTRY,0,2,0,4,0,100,3,0,0,0,0,11,24193,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Charge on Aggro');
+
+-- Wild Fel Stalker
+SET @ENTRY := 18847;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,9,0,100,2,0,5,6000,12000,11,33086,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Wild Bite on Close');
+
