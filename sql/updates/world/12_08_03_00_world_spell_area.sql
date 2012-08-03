57f3bccf997b25c0d676d74f0db6f15c477d2d37
Mikadmin <mikadmin@wow-attitude.com>
2012-08-03 10:26:13 +0200

    Fix error consol: Wrong Spell area
    I forgot remove this entry on my last fix
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_08_03_00_world_spell_area.sql
@@ -0,0 +1,7 @@
+-- Fix error consol: Wrong Spell area
+-- By Mikadmin For ARKania
+
+DELETE FROM `spell_area` WHERE `spell` = 58730 AND `area` = 4576;
+DELETE FROM `spell_area` WHERE `spell` = 58730 AND `area` = 4578;
+DELETE FROM `spell_area` WHERE `spell` = 58730 AND `area` = 4584;
+DELETE FROM `spell_area` WHERE `spell` = 58730 AND `area` = 4611;
