5c45b5c211557a63271b2d874c937894930e4f8a
Mikadmin <mikadmin@wow-attitude.com>
2012-09-27 11:54:26 +0200

    DB/CREATURE text yell Theralion
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_09_27_00_creature_text.sql
@@ -0,0 +1,12 @@
+-- Theralion text
+-- By Mikadmin For Arkania
+
+DELETE FROM `creature_text` WHERE `entry` = 45993;
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES 
+(45993, 0, 0, 'The master was clearly speaking to you, Valiona. I am far too busy to attack ANYONE !', 1, 0, 0, 0, 0, 20300, 'VO_BT_Theralion_Event01'),
+(45993, 1, 0, 'How dare you call me worthless! You will see why I am mother's favored child !', 1, 0, 0, 0, 0, 20301, 'VO_BT_Theralion_Event02'),
+(45993, 2, 0, 'Bare your soul to me, mortal.', 1, 0, 0, 0, 0, 20302, 'VO_BT_Theralion_Event03'),
+(45993, 3, 0, 'I knew I should have stayed out of this...', 1, 0, 0, 0, 0, 20303, 'VO_BT_Theralion_Event04'),
+(45993, 4, 0, 'You are not the boss of me, Valiona ! I will engulf as I please !', 1, 0, 0, 0, 0, 20304, 'VO_BT_Theralion_Event05'),
+(45993, 5, 0, 'There is no escape from the fury of the twilight flight !', 1, 0, 0, 0, 0, 20305, 'VO_BT_Theralion_Event06'),
+(45993, 6, 0, 'WRITHE IN AGONY !', 1, 0, 0, 0, 0, 20306, 'VO_BT_Theralion_Event07');
