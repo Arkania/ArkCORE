Arkania/ArkCORE

commit ce8392256200ca362d8dea7854fe55d04025d974

DB/CREATURE Sundered Emerald Colossus
Fix Spawn & Template

Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

sql/updates/world/12_10_12_00_world_creature_template.sql
@@ -1,0 +1,6 @@
+-- Sundered Emerald Colossus
+-- By Mikadmin For Arkania
+
+-- Fix Creature_Template
+
+UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82, `faction_A` = 16, `faction_H` = 16, `exp` = 3, WHERE `entry` = 44229;

sql/updates/world/12_10_12_00_world_creature.sql
@@ -1,0 +1,20 @@
+-- Sundered Emerald Colossus
+-- By Mikadmin For Arkania
+
+-- Fix spawn
+DELETE FROM `creature` WHERE `id`=44229;
+INSERT INTO `creature` (`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(44229, 646, 1, 1, 0, 0, -167.64, 945.717, 210.037, 0.25426, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, -102.911, 979.491, 211.735, 3.65582, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, -132.885, 1059.72, 221.122, 0.0775444, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, -167.79, 1118.49, 224.067, 5.7976, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 46.9325, 1099.53, 222.045, 2.56647, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 142.511, 1119.14, 228.592, 0.561348, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 14.4861, 1294.84, 220.439, 3.50659, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, -35.6468, 1385.43, 215.038, 6.11883, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 228.462, 1371.37, 224.347, 3.56471, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 260.752, 1270.09, 243.045, 3.44926, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 389.538, 1287.75, 259.541, 3.84981, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 340.212, 1171.95, 266.575, 2.08109, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 349.189, 1351, 259.578, 3.65582, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0),
+(44229, 646, 1, 1, 0, 0, 90.0319, 1446.2, 218.225, 5.2541, 300, 0, 0, 44679, 0, 0, 0, 0, 0, 0);

commit a94095bd7294ef05016452db838d730ca568e1a6

DB/CREATURE Sundered Emerald Colossus: Fix SQL Syntax error caused by ce8392256200ca362d8dea7854fe55d04025d974

sql/updates/world/12_10_12_00_world_creature_template.sql
@@ -3,4 +3,4 @@
 
 -- Fix Creature_Template
 
-UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82, `faction_A` = 16, `faction_H` = 16, `exp` = 3, WHERE `entry` = 44229;
+UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82, `faction_A` = 16, `faction_H` = 16 `exp` = 3, WHERE `entry` = 44229;