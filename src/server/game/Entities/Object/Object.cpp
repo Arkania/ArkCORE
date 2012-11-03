6db5227e0d9dada658a1273480cbd15dd28b2fb7
biglad <biglad@mgawow.co.uk>
2012-11-03 10:00:32 +0000

    Fix wintergrasp walls/door and building randomly disappear after GridUnload

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -1284,6 +1284,8 @@ void WorldObject::setActive (bool on)
             map->AddToActive(this->ToCreature());
         else if (GetTypeId() == TYPEID_DYNAMICOBJECT)
             map->AddToActive((DynamicObject*) this);
+        else if (GetTypeId() == TYPEID_GAMEOBJECT)
+            map->AddToActive((GameObject*)this);
     }
     else
     {
@@ -1291,6 +1293,8 @@ void WorldObject::setActive (bool on)
             map->RemoveFromActive(this->ToCreature());
         else if (GetTypeId() == TYPEID_DYNAMICOBJECT)
             map->RemoveFromActive((DynamicObject*) this);
+         else if (GetTypeId() == TYPEID_GAMEOBJECT)
+            map->RemoveFromActive((GameObject*)this);
     }
 }
 
