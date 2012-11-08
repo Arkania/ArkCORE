345cb05ddfadde3e261290a6140fb05cb0c752d9
Mikadmin <mikadmin@wow-attitude.com>
2012-11-08 14:23:15 +0100

    DB/QUEST Honor Challenge
    Fix count npc challenged
    
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_11_08_00_quest_template.sql
@@ -0,0 +1,5 @@
+-- Honor Challenge
+-- By mikadmin [www.wow-attitude.com]
+-- Fix count npc challenged
+
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 30037, `ReqSpellCast1` = 21855 WHERE `entry` = 12939;
