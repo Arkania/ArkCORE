516a95115e9b22a38b1e560df705849ef2f9fb36
Mikadmin <mikadmin@wow-attitude.com>
2012-11-09 11:24:20 +0100

    Upgrade my fix Sethria
    thx label for good convert health and mana
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

--- sql/updates/world/12_11_08_01_creature_template.sql
+++ sql/updates/world/12_11_08_01_creature_template.sql
@@ -4,4 +4,4 @@
 
 SET @ENTRY := 41255;
 
-UPDATE `creature_template` SET `maxlevel` = 81, `exp` = 3, `Health_mod` = 4.3295, `Mana_mod` = 1.4835 WHERE entry=@ENTRY;
+UPDATE `creature_template` SET `maxlevel` = 81, `exp` = 3, `Health_mod` = 10, `Mana_mod` = 2 WHERE entry=@ENTRY;
