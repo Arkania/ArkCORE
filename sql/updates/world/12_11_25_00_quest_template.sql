9c33fe595e285919bdead3f3cd064dfce8d2b400
Mikadmin <mikadmin@wow-attitude.com>
2012-11-25 19:59:57 +0100

    DB/QUEST Moonfire
    Fix wrong detail
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_11_25_00_quest_template.sql
@@ -0,0 +1,4 @@
+-- Moonfire
+-- By Mikadmin For Arkania
+
+UPDATE `quest_template` SET `RequiredRaces` = 8, `ReqCreatureOrGOId1` = 44614, `ReqCreatureOrGOCount1` = 1, `ReqSpellCast1` = 8921, `Title` = 'Moonfire', `Details` = 'Druids play an important role in our society, but all too often we are seen as passive dreamers in a world that is changing around us.$B$BThe truth is, though, we must actively practice and go out onto the world to develop our connection to nature. So go, get some more experience. You will begin to feel your connection deepen, learning new skills. Demonstrate the first you learn for me. ', `Objectives` = 'Reach level 3 to learn Moonfire. Use it on a training dummy in Aldrassil. ', `ObjectiveText1` = 'Reach Level 3 to Learn Moonfire', `RequiredSpell` = 8921 WHERE `entry` = 26948;
