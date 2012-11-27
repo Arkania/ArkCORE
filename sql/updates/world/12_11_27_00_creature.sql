cba06e7b53d7c8a8012bcd60e2962b9730bf2510
Mikadmin <mikadmin@wow-attitude.com>
2012-11-27 16:51:25 +0100

    DB/Creature Bhag'thera
    Fix spawn position
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_11_27_00_creature.sql
@@ -0,0 +1,6 @@
+-- Bhag'thera
+-- By Mikadmin for Arkania
+
+DELETE FROM `creature` WHERE `id`=728;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(1348, 728, 0, 1, 1, 613, 0, -12181.884766, -946.364685, 40.343132, 0.187693, 600, 0, 0, 1753, 0, 0, 0, 0, 0, 0);
