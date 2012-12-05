90a1b2d9e8b20891c225a1d179778f61abfd143d
Mikadmin <mikadmin@wow-attitude.com>
2012-12-05 16:57:47 +0100

    DB/creature_template more fix skettis
    Fix Terokk's Downfall suite
    
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_12_05_00_creature_template.sql
@@ -0,0 +1,8 @@
+-- more fix skettis
+-- By Mikadmin For Arkania
+-- Fix Terokk's Downfall suite
+
+UPDATE `creature_template` SET `faction_A` = 1862, `faction_H` = 1862, `dynamicflags` = 8 WHERE `entry` = 23161;
+UPDATE `creature_template` SET `faction_A` = 1862, `faction_H` = 1862, `unit_flags` = 0, `dynamicflags` = 8 WHERE `entry` = 23162;
+UPDATE `creature_template` SET `faction_A` = 1862, `faction_H` = 1862, `dynamicflags` = 8 WHERE `entry` = 23163;
+UPDATE `creature_template` SET `faction_A` = 1862, `faction_H` = 1862, `dynamicflags` = 8 WHERE `entry` = 23165;
