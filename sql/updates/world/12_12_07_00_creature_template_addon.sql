15ae098de2b691df7d7ab9d34574b21f7dba9575
Mikadmin <mikadmin@wow-attitude.com>
2012-12-07 16:51:25 +0100

    DB/CREATURE Fix wrong aura Alysra
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

--- sql/updates/world/12_12_07_00_creature_template_addon.sql
+++ sql/updates/world/12_12_07_00_creature_template_addon.sql
@@ -3,3 +3,7 @@
 
 DELETE FROM `creature_template_addon` WHERE (`entry`=41504);
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (41504, 0, 0, 65536, 1, 0, '');
+
+-- Fix wrong aura Alysra
+DELETE FROM `creature_template_addon` WHERE (`entry`=38917);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (38917, 0, 0, 50397184, 1, 0, '');
