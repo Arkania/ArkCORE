1fd828b77935efb1ae04fa4c20113943cdb37cf8
Mikadmin <mikadmin@wow-attitude.com>
2012-12-07 12:25:30 +0100

    DB/GAMEOBJECT Fix Portal Uldum
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_12_07_00_gameobject_scripts.sql
@@ -0,0 +1,5 @@
+-- Fix Portal Uldum
+-- By Mikadmin Fopr Arkania
+
+UPDATE `gameobject_scripts` SET `id` = 4017959 WHERE `entry` = 50010;
+UPDATE `gameobject_scripts` SET `id` = 4018087 WHERE `entry` = 50011;
---
+++ sql/updates/world/12_12_07_00_gameobject_template.sql
@@ -0,0 +1,5 @@
+-- Fix Portal Uldum
+-- By Mikadmin Fopr Arkania
+
+UPDATE `gameobject_template` SET `type` = 1, `data0` = 0, `data2` = 3000, `data5` = 0 WHERE `entry` = 207695;
+UPDATE `gameobject_template` SET `type` = 1, `data0` = 0, `data2` = 3000, `data5` = 0 WHERE `entry` = 207687;
