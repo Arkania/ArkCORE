67cf9a5009be7e0c6c80fa15e5500ccf4d70be0e
Mikadmin <mikadmin@wow-attitude.com>
2012-12-07 16:47:24 +0100

    DB/CREATURE Fix wrong aura Tortolla
    Signed-off-by: Mikadmin <mikadmin@wow-attitude.com>

---
+++ sql/updates/world/12_12_07_00_creature_template_addon.sql
@@ -0,0 +1,5 @@
+-- Fix wrong aura Tortolla
+-- By Mikadmin for arkania
+
+DELETE FROM `creature_template_addon` WHERE (`entry`=41504);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES (41504, 0, 0, 65536, 1, 0, '');
