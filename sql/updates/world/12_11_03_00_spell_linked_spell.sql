321ed721fc14b121cc18033d7be9155d35225cb9
grsgs <jorjescu@ymail.com>
2012-12-08 23:08:56 +0200

    Update sql/updates/world/12_11_03_00_spell_linked_spell.sql

--- sql/updates/world/12_11_03_00_spell_linked_spell.sql
+++ sql/updates/world/12_11_03_00_spell_linked_spell.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`='12975';
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('12975', '12976', 'Warrior Last Stand');
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`='53478';
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('53478', '53479', 'Huner Last Stand');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('53478', '53479', 'Hunter Last Stand');
