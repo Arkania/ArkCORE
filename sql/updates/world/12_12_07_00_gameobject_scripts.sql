8980d0a9671f337903e438a2cb6f5f255f4919a6
Mikadmin <mikadmin@wow-attitude.com>
2012-12-09 21:35:47 +0100

    fix error name column

--- sql/updates/world/12_12_07_00_gameobject_scripts.sql
+++ sql/updates/world/12_12_07_00_gameobject_scripts.sql
@@ -1,5 +1,5 @@
 -- Fix Portal Uldum
 -- By Mikadmin Fopr Arkania
 
-UPDATE `gameobject_scripts` SET `id` = 4017959 WHERE `entry` = 50010;
-UPDATE `gameobject_scripts` SET `id` = 4018087 WHERE `entry` = 50011;
+UPDATE `gameobject_scripts` SET `id` = 4017959 WHERE `id` = 50010;
+UPDATE `gameobject_scripts` SET `id` = 4018087 WHERE `id` = 50011;
